49c1e4bf17066c2ed657170e28979e785d7923cc
Matthew <mdg583@gmail.com>
2017-08-03 09:40:50 +0300

    should sort orders before processing

--- pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/services/HL7Service.java
+++ pacs-integration-webapp/src/main/java/org/bahmni/module/pacsintegration/services/HL7Service.java
@@ -82,7 +82,7 @@ public class HL7Service {
     private AbstractMessage cancelOrderMessage(OpenMRSOrder order, OpenMRSPatient openMRSPatient, List<OpenMRSProvider> providers) throws DataTypeException {
         Order previousOrder = orderRepository.findByOrderUuid(order.getPreviousOrderUuid());
         if(previousOrder == null) {
-            throw  new HL7MessageException("Unable to Cancel the Order. Previous order is not found" + order.getOrderNumber());
+            throw  new HL7MessageException("Unable to Cancel the Order. Previous order is not found " + order.getOrderNumber());
         }
         ORM_O01 message = new ORM_O01();
         addMessageHeader(order, message);
